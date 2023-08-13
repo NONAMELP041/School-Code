760ef57903aa923894f5456542dae61afc9d432a
NONAMELP041 <erictubehd84@gmail.com>
2023-08-13 17:08:02 -0500

    AllAboutMe

---
+++ AllAboutMe.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AllAboutMe
+{
+    public static void main(String[] args) {
+        System.out.println("what is your name?,\n My name is Eric");
+        System.out.println("What is your age?,\n My age is 17");
+        System.out.println("How many siblings do you have?,\n I have 1 sibling, a younger brother");
+        System.out.println("What is you favorite song?,\n My favorite song is Queencard by GIDLE");
+        System.out.println("What is your favorite food?\n My favorite food is Sushi");
+        System.out.println("What is your favorite movie?\n My favorite movie is Kimi No Nawa");    
+    }
+}
